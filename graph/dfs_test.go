3179bd98612adcb0ee96ab88927f5d8c60f89b4b
dongbo7 <dongbo7@staff.weibo.com>
2022-07-14 14:22:12 +0800

    feat: add full permutation unit test

--- graph/dfs_test.go
+++ graph/dfs_test.go
@@ -1,14 +1,19 @@
 package graph
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestPartialSummation(t *testing.T) {
 	nums := []int{1, 3, 5, 6, 10}
-	fmt.Println(PartialSummation(nums, 15))
-	fmt.Println(PartialSummation(nums, 11))
-	fmt.Println(PartialSummation(nums, 1))
-	fmt.Println(PartialSummation(nums, 20))
+	t.Log(PartialSummation(nums, 15))
+	t.Log(PartialSummation(nums, 11))
+	t.Log(PartialSummation(nums, 1))
+	t.Log(PartialSummation(nums, 20))
+}
+
+func TestFullPermutation(t *testing.T) {
+	t.Logf("words: %v, full permutation: %v", []string{"hello", "world"}, FullPermutation([]string{"hello", "world"}))
+	t.Logf("words: %v, full permutation: %v", []string{"hello", "world", "this", "is", "for", "you"}, FullPermutation([]string{"hello", "world", "this", "is", "for", "you"}))
+
 }
